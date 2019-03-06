bb531c64ab6aaa5fe3ad294dfd622f5296e5192f
Arianxx <34032486+Arianxx@users.noreply.github.com>
2019-03-07 07:34:00 +0800

    docs: update middleware exapmle

--- examples/middleware/__init__.py
+++ examples/middleware/__init__.py
@@ -6,17 +6,23 @@ app = Freesia()
 @app.route("/<name>")
 async def hello(request, name):
     print("enter user handler")
-    return "hello, " + name + "!"
+    return "hello, " + name
 
 
-async def add_smiling_face(request, handler):
-    print("enter middleware")
+async def middleware1(request, handler):
+    print("enter middleware1")
     res = await handler()
-    print("exit middleware")
-    return res + " :D"
+    print("exit middleware1")
+    return res + " !"
 
 
-app.use([add_smiling_face])
+async def middleware2(request, handler):
+    print("enter middleware2")
+    res = await handler()
+    print("exit middleware2")
+
+
+app.use([middleware1, middleware2])
 
 if __name__ == "__main__":
     app.run()
